2a55ab429e1596017f969e5bc096a5f541d1b579
leducanh <ducanh@olli-ai.com>
2022-10-11 16:03:43 +0700

    upgrade sdk 33

--- widget/src/main/java/com/ittianyu/bottomnavigationviewex/BottomNavigationViewInner.java
+++ widget/src/main/java/com/ittianyu/bottomnavigationviewex/BottomNavigationViewInner.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.support.design.internal.BottomNavigationItemView;
-import android.support.design.internal.BottomNavigationMenuView;
-import android.support.design.internal.ThemeEnforcement;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.view.ViewPager;
-import android.support.v7.widget.TintTypedArray;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.internal.ThemeEnforcement;
+import com.google.android.material.navigation.NavigationBarView;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.widget.TintTypedArray;
 import android.util.AttributeSet;
 import android.util.SparseIntArray;
 import android.util.TypedValue;
@@ -64,12 +66,12 @@ public class BottomNavigationViewInner extends BottomNavigationView {
     public BottomNavigationViewInner(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         TintTypedArray a = ThemeEnforcement.obtainTintedStyledAttributes(context, attrs,
-                android.support.design.R.styleable.BottomNavigationView,
-                defStyleAttr, android.support.design.R.style.Widget_Design_BottomNavigationView,
-                new int[]{android.support.design.R.styleable.BottomNavigationView_itemTextAppearanceInactive,
-                        android.support.design.R.styleable.BottomNavigationView_itemTextAppearanceActive});
+                R.styleable.BottomNavigationView,
+                defStyleAttr, R.style.Widget_Design_BottomNavigationView,
+                new int[]{R.styleable.NavigationBarView_itemTextAppearanceInactive,
+                        R.styleable.NavigationBarView_itemTextAppearanceActive});
         // clear if you don't have set item icon tint list
-        if (!a.hasValue(android.support.design.R.styleable.BottomNavigationView_itemIconTint)) {
+        if (!a.hasValue(R.styleable.NavigationView_itemIconTint)) {
             clearIconTintColor();
         }
         a.recycle();
@@ -333,7 +335,7 @@ public class BottomNavigationViewInner extends BottomNavigationView {
         // 2. change field mShiftingMode value in mMenuView
 //        setField(mMenuView.getClass(), mMenuView, "isShifting", enable);
 //        mMenuView.updateMenuView();
-        setLabelVisibilityMode(enable ? 0 : 1);
+        setLabelVisibilityMode(enable ? NavigationBarView.LABEL_VISIBILITY_SELECTED : NavigationBarView.LABEL_VISIBILITY_LABELED);
         return this;
     }
 
